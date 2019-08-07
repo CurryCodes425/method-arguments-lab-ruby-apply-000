1fcf146ee3f0d4ec96cb9d8a9c07a89a6f1678c9
Casey (Bevin) Curry <c.larsoncurry425@gmail.com>
2019-08-07 06:41:23 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -5,5 +5,5 @@ end
 
 prog_name = "Ruby.rb"
 def introduction_with_language(name, prog_name)
-  puts "Hi, my name is #{name} and I am learning to program in #{prog_name}"
+  puts "Hi, my name is #{name} and I am learning to program in #{prog_name}\n"
 end
