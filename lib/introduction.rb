87e16f297dcc518ee92fbe273c91d3df65c735f3
Casey (Bevin) Curry <c.larsoncurry425@gmail.com>
2019-08-07 06:45:19 +0000

    Done.

---
+++ lib/introduction.rb
@@ -0,0 +1,10 @@
+name = "Casey"
+def introduction(name)
+  puts "Hi, my name is #{name}."
+end
+
+prog_name = "Ruby.rb\n"
+name = "Casey"
+def introduction_with_language(name, prog_name)
+  puts "Hi, my name is #{name} and I am learning to program in #{prog_name}."
+end
